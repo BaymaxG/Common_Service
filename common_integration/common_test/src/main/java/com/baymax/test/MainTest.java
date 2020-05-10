a9793a4b8ca026dc0de0093e9584246e206f1653
Baymax <1592331270@qq.com>
2020-05-10 16:25:46 +0800

    [desc]master修改 [expand]NA

--- common_integration/common_test/src/main/java/com/baymax/test/MainTest.java
+++ common_integration/common_test/src/main/java/com/baymax/test/MainTest.java
@@ -4,15 +4,22 @@
 
 package com.baymax.test;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author guNingbo
  * @since 2020/5/10
  */
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println(">>=========================<<");
-        System.out.println(">>=========================<<");
-        System.out.println(">>=========================<<");
-        System.out.println(">>=========================<<");
+        List<String> list = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(list)) {
+            list.forEach(System.out::println);
+        } else {
+            System.out.println("list is null!");
+        }
     }
 }
