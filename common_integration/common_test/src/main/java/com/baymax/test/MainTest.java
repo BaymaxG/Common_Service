02f4856296aafb05951c859f81a30ffbbf2125b1
Baymax <1592331270@qq.com>
2020-05-10 15:47:04 +0800

    [desc]分支学习 [expand]NA

---
+++ common_integration/common_test/src/main/java/com/baymax/test/MainTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2020-2020 BaymaxG.guNingbo
+ */
+
+package com.baymax.test;
+
+/**
+ * @author guNingbo
+ * @since 2020/5/10
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println(">>=========================<<");
+    }
+}
